6192a3b19555577e3c43e67214c3bcb841b25998
AtomMushroom <35073893+AtomMushroom@users.noreply.github.com>
2022-02-17 19:30:20 +0300

    Add files via upload

---
+++ Lesson_5Task7/L5Task7.cs
@@ -0,0 +1,23 @@
+﻿int positive = 0;
+int negative = 0;
+
+PosNegSeq(ref positive, ref negative);
+
+static void PosNegSeq(ref int positive, ref int negative)
+{
+    int number = 0;
+    do
+    {
+        number = int.Parse(Console.ReadLine());
+        if (number>0)
+        {
+            positive++;
+        }
+        else if (number<0)
+        {
+            negative++;
+        }
+    } while (number!=0);
+    Console.WriteLine($"Положительные: {positive}");
+    Console.WriteLine($"Отрицательные: {negative}");
+}
